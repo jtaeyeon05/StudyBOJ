a98959f1642a937f423124d6b43871f0ad083de4
jtaeyeon05 <113190558+jtaeyeon05@users.noreply.github.com>
2026-08-30 13:53:39 +0000

    Solve: [LeetCode/Medium] 7. Reverse Integer (Python3)

---
+++ LeetCode/Medium/leetcode_0007_reverse_integer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = 1 
+        if x < 0:
+            sign = -1
+            x = -x
+    
+        result = 0
+        while x != 0:
+            result = result * 10 + x % 10
+            x //= 10
+        result *= sign
+
+        if result < -(2 ** 31) or result > 2 ** 31 - 1:
+            return 0
+        return result
+        
